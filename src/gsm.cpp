a9dcc9e7a624018e253d46661ad2beec0bd76285
Kazaiyki <maximostapiyk44@gmail.com>
2023-06-14 02:10:04 +0300

    Returned SendData function to delay from millis

--- src/gsm.cpp
+++ src/gsm.cpp
@@ -6,122 +6,63 @@ unsigned long preview_time_gsm;
 void SendData(float h, float t, float s, float d){
 if (Serial1.available())
     Serial.write(Serial1.read());
+  
+    Serial1.println("AT");
+  delay(1000);
 
-    current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT");
-    }
-  preview_time_gsm = millis();
+    Serial1.println("AT+CPIN?");
+  delay(1000);
 
-  current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CPIN?");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CREG?");
+  delay(1000);
 
- current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CREG?");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CGATT?");
+  delay(1000);
 
- current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CGATT?");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIPSHUT");
+  delay(1000);
 
-current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CIPSHUT");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIPSTATUS");
+  delay(2000);
 
-current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CIPSTATUS");
-    }
-  preview_time_gsm = millis();
-
-current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CIPMUX=0");
-    }
-  preview_time_gsm = millis();
-
-  current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CSTT=\"internet\"");
-    }
-  preview_time_gsm = millis();
-
-  current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_SEND_DELAY)
-    {
-        Serial1.println("AT+CIICR");
-    }
-  preview_time_gsm = millis();
-
- current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_DELAY)
-    {
-       Serial1.println("AT+CIFSR");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIPMUX=0");
+  delay(2000);
 
 
-   current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_SEND_DELAY)
-    {
-       Serial1.println("AT+CIPSPRT=0");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CSTT=\"internet\""); // start task and setting the APN,
+  delay(1000);
 
-   current_time_gsm = millis();
-  if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_CONNECTON_DELAY)
-    {
-       Serial1.println("AT+CIPSPRT=0");
-    }
-  preview_time_gsm = millis();
 
-    if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_CONNECTON_DELAY)
-    {
-       Serial1.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIICR"); // bring up wireless connection
+  delay(3000);
 
-      if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_SEND_DELAY)
-    {
-       Serial1.println("AT+CIPSEND");
-    }
-  preview_time_gsm = millis();
 
-      if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_SEND_DELAY)
-    {
-       String str = "GET https://api.thingspeak.com/update?api_key=RLHRL8UF3X65GQH6&field1=" + String(h) + "&field2=" + String(t) + "&field3=" + String(s) + "&field4=" + String(d) ;
-        Serial.println(str);
-        Serial1.println(str);
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIFSR"); // get local IP adress
+  delay(2000);
 
-      if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_CONNECTON_DELAY)
-    {
-        Serial1.println((char)26);
-    }
-  preview_time_gsm = millis();
 
-        if (((current_time_gsm - preview_time_gsm) / 1000) >= GSM_CONNECTON_DELAY)
-    {
-        Serial1.println("AT+CIPSHUT");
-    }
-  preview_time_gsm = millis();
+  Serial1.println("AT+CIPSPRT=0");
+  delay(3000);
+
+
+  Serial1.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\""); // start up the connection
+  delay(6000);
+
+
+  Serial1.println("AT+CIPSEND"); // begin send data to remote server
+  delay(4000);
+
+  String str = "GET https://api.thingspeak.com/update?api_key=RLHRL8UF3X65GQH6&field1=" + String(h) + "&field2=" + String(t) + "&field3=" + String(s) + "&field4=" + String(d) ;
+  Serial.println(str);
+  Serial1.println(str); // begin send data to remote server
+
+  delay(4000);
+
+  Serial1.println((char)26); // sending
+  delay(5000);               // waitting for reply, important! the time is base on the condition of internet
+  Serial1.println();
+
+  Serial1.println("AT+CIPSHUT"); // close the connection
+  delay(100);
 }
 
